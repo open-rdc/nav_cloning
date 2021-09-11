ea2388f816496d94ed0bbba7154aa707881ca738
MasayaOkada <masa.okd888@gmail.com>
2021-09-11 23:39:02 +0900

    add move_change cov

---
+++ scripts/nav_cloning_navigation.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import roslib
+roslib.load_manifest('nav_cloning')
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from nav_cloning_net import *
+from skimage.transform import resize
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PoseArray
+from std_msgs.msg import Int8
+from std_srvs.srv import Trigger
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from std_srvs.srv import Empty
+from std_srvs.srv import SetBool, SetBoolResponse
+import csv
+import os
+import time
+import copy
+
+class cource_following_learning_node:
+    def __init__(self):
+        rospy.init_node('cource_following_learning_node', anonymous=True)
+        self.action_num = rospy.get_param("/LiDAR_based_learning_node/action_num", 1)
+        print("action_num: " + str(self.action_num))
+        self.dl = deep_learning(n_action = self.action_num)
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.callback)
+        self.vel_sub = rospy.Subscriber("/nav_vel", Twist, self.callback_vel)
+        self.action_pub = rospy.Publisher("action", Int8, queue_size=1)
+        self.nav_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.srv = rospy.Service('/training', SetBool, self.callback_dl_training)
+        self.pose_sub = rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.callback_pose)
+        self.path_sub = rospy.Subscriber("/move_base/NavfnROS/plan", Path, self.callback_path)
+        self.min_distance = 0.0
+        self.cov = 0.0
+        self.action = 0.0
+        self.vel = Twist()
+        self.path_pose = PoseArray()
+        self.cv_image = np.zeros((480,640,3), np.uint8)
+        self.model_base = False
+        self.select_dl = False
+        self.start_time = time.strftime("%Y%m%d_%H:%M:%S")
+        self.path = roslib.packages.get_pkg_dir('nav_cloning') + '/data/result/'
+        self.save_path = roslib.packages.get_pkg_dir('nav_cloning') + '/data/model/'
+        self.previous_reset_time = 0
+        self.start_time_s = rospy.get_time()
+
+    def callback(self, data):
+        try:
+            self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+    def callback_path(self, data):
+        self.path_pose = data
+
+    def callback_pose(self, data):
+        distance_list = []
+        pos = data.pose.pose.position
+        self.cov = data.pose.covariance[0] + data.pose.covariance[7]
+
+        for pose in self.path_pose.poses:
+            path = pose.pose.position
+            distance = np.sqrt(abs((pos.x - path.x)**2 + (pos.y - path.y)**2))
+            distance_list.append(distance)
+
+        if distance_list:
+            self.min_distance = min(distance_list)
+
+    def callback_vel(self, data):
+        self.vel = data
+        self.action = self.vel.angular.z
+
+    def callback_dl_training(self, data):
+        resp = SetBoolResponse()
+        self.learning = data.data
+        resp.message = "Training: " + str(self.learning)
+        resp.success = True
+        return resp
+
+    def loop(self):
+        if self.cv_image.size != 640 * 480 * 3:
+            return
+
+        if self.vel.linear.x == 0:
+            return
+
+        img = resize(self.cv_image, (48, 64), mode='constant')
+        r, g, b = cv2.split(img)
+        imgobj = np.asanyarray([r,g,b])
+
+        ros_time = str(rospy.Time.now())
+        self.dl.load()
+
+        if self.cov < 1:
+            self.model_base = False
+        else:
+            self.model_base = True
+
+        if self.model_base:
+            target_action = self.dl.act(imgobj)
+            print("TEST MODE: " + " angular:" + str(target_action))
+            self.vel.linear.x = 0.2
+            self.vel.angular.z = target_action
+            self.nav_pub.publish(self.vel)
+
+        else:
+            target_action = self.action
+            print("NAVIGSTION MODE: " + " angular:" + str(target_action))
+            self.vel.linear.x = 0.2
+            self.vel.angular.z = target_action
+            self.nav_pub.publish(self.vel)
+
+        temp = copy.deepcopy(img)
+        cv2.imshow("Resized Image", temp)
+
+if __name__ == '__main__':
+    rg = cource_following_learning_node()
+    DURATION = 0.2
+    r = rospy.Rate(1 / DURATION)
+    while not rospy.is_shutdown():
+        rg.loop()
+        r.sleep()
