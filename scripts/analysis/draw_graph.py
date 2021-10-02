c99ffed7d35cdb96f8cf86f4130e7c6553abb6cb
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2021-10-03 07:06:39 +0900

    add map

--- scripts/analysis/draw_graph.py
+++ scripts/analysis/draw_graph.py
@@ -5,16 +5,21 @@ roslib.load_manifest('nav_cloning')
 import rospy
 import csv
 import math
-
 from matplotlib import pyplot
 from matplotlib.patches import ArrowStyle
 import numpy as np
+from PIL import Image
 
 def arrow():
     rospy.init_node('arrow_node', anonymous=True)
     path = roslib.packages.get_pkg_dir('nav_cloning') + '/data/analysis/'
+    image = Image.open(roslib.packages.get_pkg_dir('nav_cloning')+'/maps/map.png').convert("L")
+    arr = np.asarray(image)
+
     fig = pyplot.figure()
     ax = fig.add_subplot(111)
+
+    ax.imshow(arr, cmap='gray', extent=[-10,50,-10,50])
     arrow_dict = dict(arrowstyle = "->", color = "black")
     with open(path + 'result.csv', 'r') as f:
         for row in csv.reader(f):
@@ -24,9 +29,8 @@ def arrow():
             y1 = y+0.3*math.sin(the+ang_vel*2)
             ax.annotate('', xy=(x1, y1), xytext=(x, y), arrowprops = arrow_dict)
 
-    ax.set_xlim([-12, 17])
-    ax.set_ylim([-20, 0])
-
+    ax.set_xlim([-5, 30])
+    ax.set_ylim([-5, 15])
     pyplot.show()
 
 if __name__ == '__main__':
