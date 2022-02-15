67a0471dc5a7be7209ac9b0bd6d1c83b72f80f8e
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2022-02-15 18:43:22 +0900

    add experimental environment

---
+++ experiments/experiment_cit3f_with_direction_selected_training.sh
@@ -0,0 +1,5 @@
+for i in `seq 1`
+do
+  roslaunch nav_cloning nav_cloning_sim.launch script:=nav_cloning_with_direction_node.py mode:=selected_training world_name:=Tsudanuma_2-3.world map_file:=cit_3f_map.yaml waypoints_file:=cit3f_way.yaml dist_err:=1.0 initial_pose_x:=-10.0 initial_pose_y:=2.5 initial_pose_a:=-1.57 use_waypoint_nav:=true robot_x:=-10.0 robot_y:=2.5 robot_Y:=-1.57
+  sleep 10
+done
