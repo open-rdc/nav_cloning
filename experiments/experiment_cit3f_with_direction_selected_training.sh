ed18f3d49de0785843695c2db4c413b337052eb1
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2022-02-18 18:01:38 +0900

    change cit3f path

--- experiments/experiment_cit3f_with_direction_selected_training.sh
+++ experiments/experiment_cit3f_with_direction_selected_training.sh
@@ -1,5 +1,5 @@
 for i in `seq 1`
 do
-  roslaunch nav_cloning nav_cloning_sim.launch script:=nav_cloning_with_direction_node.py mode:=selected_training world_name:=Tsudanuma_2-3.world map_file:=cit_3f_map.yaml waypoints_file:=cit3f_way.yaml dist_err:=1.0 initial_pose_x:=-10.0 initial_pose_y:=2.5 initial_pose_a:=-1.57 use_waypoint_nav:=true robot_x:=-10.0 robot_y:=2.5 robot_Y:=-1.57
+  roslaunch nav_cloning nav_cloning_sim.launch script:=nav_cloning_with_direction_node.py mode:=selected_training world_name:=Tsudanuma_2-3.world map_file:=cit_3f_map.yaml waypoints_file:=cit3f_way.yaml dist_err:=1.0 initial_pose_x:=-5.0 initial_pose_y:=7.7 initial_pose_a:=3.14 use_waypoint_nav:=true robot_x:=-5.0 robot_y:=7.7 robot_Y:=3.14
   sleep 10
 done
