e1e0d905565e325ab6f4010041cccd17091cfa3a
haruyama8940 <yaruo0406@gmail.com>
2022-04-27 15:08:24 +0900

    fix

--- scripts/nav_cloning_net.py
+++ scripts/nav_cloning_net.py
@@ -24,20 +24,10 @@ from paramiko import Channel
 import torch
 import torchvision
 import torch.nn as nn
-#import torch.nn.functional as F
-#import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset, random_split
+from torch.utils.data import DataLoader, TensorDataset, Dataset, random_split
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
-
-# torch関連ライブラリのインポート
-
-import torch
-import torch.nn as nn
-import torch.functional as F
 import torch.optim as optim
-#from torchinfo import summary
-#from torchviz import make_dot
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
@@ -53,25 +43,34 @@ class Net(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(n_channel, 32,kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(32,64,kernel_size=3, stride=2)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d()
-        self.flatten = nn.Flatten()
+        self.conv3 = nn.Conv2d(64,64, kernel_size=3, stride=1)
         self.fc4 = nn.Linear(960, 512)
         self.fc5 = nn.Linear(512,n_out)
-    
-        self.features = nn.Sequential(
+        self.relu = nn.ReLU(inplace=True)
+        
+        self.maxpool = nn.MaxPool2d()
+        self.batch = nn.BatchNorm2d(64)
+        self.flatten = nn.Flatten()
+        
+        self.cnn_layer = nn.Sequential(
             self.conv1,
             self.relu,
             self.conv2,
             self.relu,
+            self.conv3,
+            self.relu,
+            self.flatten
+        )
+        self.fc_layer = nn.Sequential(
             self.fc4,
             self.relu,
             self.fc5
         )
 
     def forward(self,x):
-        x1 = self.features(x)
-        return x1
+        x1 = self.cnn_layer(x)
+        x2 = self.fc_layer(x1)
+        return x2
 
 class deep_learning:
     def __init__(self, n_channel=3, n_action=1):
@@ -79,6 +78,7 @@ class deep_learning:
         self.net = Net(n_channel, n_action).to(device)
         self.optimizer = optim.Adam(eps=1e-2,weight_decay=5e-4)
         self.optimizer.setup(self.net.parameters())
+
         self.n_action = n_action
         self.count = 0
         self.accuracy = 0
@@ -94,33 +94,31 @@ class deep_learning:
             self.net.train()
             x = torch.tensor(imgobj).float()
             t = torch.tensor(target_angle).float()
-            self.img_data.append(x[0])
-            self.target_angles.append(t[0])
+            # self.img_data.append(x[0])
+            # self.target_angles.append(t[0])
 
             if len(self.img_data) > MAX_DATA:
                 del self.img_data[0]
                 del self.target_angles[0]
-            dataset = Dataset(x,t)
+            dataset = TensorDataset(x,t)
             train_dataset = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
             x_train, t_train = random_split(train_dataset)
+
             y_train = self.net(x_train)
             loss_train = self.criterion(y_train, t_train) #loss
-            self.loss_list.append(loss_train.array)
-            self.net.cleargrads()
             loss_train.backward()
             self.optimizer.step()
-            
+            self.optimizer.zero_grad
             self.count += 1
-            self.results_train['loss'] .append(loss_train.array)
             x_test = x
             action_value = self.net(x_test)
-            return action_value.data[0][0], loss_train.array
+            return action_value.item(), loss_train.item()
 
     def act(self, imgobj):
             self.net.eval()
             x_test = torch.tensor(imgobj)
             action_value = self.net(x_test)
-            return action_value.data[0][0]
+            return action_value.item()
 
     def result(self):
             accuracy = self.accuracy
