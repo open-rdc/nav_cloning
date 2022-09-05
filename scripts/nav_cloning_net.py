747c54f5bfe25944a835ff67da5989c37ba4a959
MasakiFujiwara <fmasaki2020@gmail.com>
2022-09-05 23:37:21 +0900

    fix for load model

--- scripts/nav_cloning_net.py
+++ scripts/nav_cloning_net.py
@@ -23,17 +23,18 @@ from yaml import load
 BATCH_SIZE = 8
 MAX_DATA = 10000
 
+
 class Net(nn.Module):
     def __init__(self, n_channel, n_out):
         super().__init__()
-    #<Network CNN 3 + FC 2> 
-        self.conv1 = nn.Conv2d(n_channel, 32,kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(32,64,kernel_size=3, stride=2)
-        self.conv3 = nn.Conv2d(64,64, kernel_size=3, stride=1)
+    # <Network CNN 3 + FC 2>
+        self.conv1 = nn.Conv2d(n_channel, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
         self.fc4 = nn.Linear(960, 512)
-        self.fc5 = nn.Linear(512,n_out)
+        self.fc5 = nn.Linear(512, n_out)
         self.relu = nn.ReLU(inplace=True)
-    #<Weight set>
+    # <Weight set>
         torch.nn.init.kaiming_normal_(self.conv1.weight)
         torch.nn.init.kaiming_normal_(self.conv2.weight)
         torch.nn.init.kaiming_normal_(self.conv3.weight)
@@ -42,7 +43,7 @@ class Net(nn.Module):
         #self.maxpool = nn.MaxPool2d(2,2)
         #self.batch = nn.BatchNorm2d(0.2)
         self.flatten = nn.Flatten()
-    #<CNN layer>   
+    # <CNN layer>
         self.cnn_layer = nn.Sequential(
             self.conv1,
             self.relu,
@@ -50,10 +51,10 @@ class Net(nn.Module):
             self.relu,
             self.conv3,
             self.relu,
-            #self.maxpool,
+            # self.maxpool,
             self.flatten
         )
-    #<FC layer (output)>
+    # <FC layer (output)>
         self.fc_layer = nn.Sequential(
             self.fc4,
             self.relu,
@@ -61,21 +62,24 @@ class Net(nn.Module):
 
         )
 
-    #<forward layer>
-    def forward(self,x):
+    # <forward layer>
+    def forward(self, x):
         x1 = self.cnn_layer(x)
         x2 = self.fc_layer(x1)
         return x2
 
+
 class deep_learning:
     def __init__(self, n_channel=3, n_action=1):
-        #<tensor device choiece>
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # <tensor device choiece>
+        self.device = torch.device(
+            'cuda' if torch.cuda.is_available() else 'cpu')
         self.net = Net(n_channel, n_action)
         self.net.to(self.device)
         print(self.device)
-        self.optimizer = optim.Adam(self.net.parameters(),eps=1e-2,weight_decay=5e-4)
-        #self.optimizer.setup(self.net.parameters())
+        self.optimizer = optim.Adam(
+            self.net.parameters(), eps=1e-2, weight_decay=5e-4)
+        # self.optimizer.setup(self.net.parameters())
         self.totensor = transforms.ToTensor()
         self.n_action = n_action
         self.count = 0
@@ -87,110 +91,113 @@ class deep_learning:
         self.datas = []
         self.target_angles = []
         self.criterion = nn.MSELoss()
-        self.transform=transforms.Compose([transforms.ToTensor()])
-        self.first_flag =True
+        self.transform = transforms.Compose([transforms.ToTensor()])
+        self.first_flag = True
         torch.backends.cudnn.benchmark = False
         #self.writer = SummaryWriter(log_dir="/home/haru/nav_ws/src/nav_cloning/runs",comment="log_1")
 
-    def act_and_trains(self, img,target_angle):
-        
-        #<training mode> 
-            self.net.train()
+    def act_and_trains(self, img, target_angle):
 
-            if self.first_flag:
-                self.x_cat = torch.tensor(img,dtype=torch.float32, device=self.device).unsqueeze(0)
-                self.x_cat=self.x_cat.permute(0,3,1,2)
-                self.t_cat = torch.tensor([target_angle],dtype=torch.float32,device=self.device).unsqueeze(0)
-                self.first_flag =False
-            # x= torch.tensor(self.transform(img),dtype=torch.float32, device=self.device).unsqueeze(0)
-        #<to tensor img(x),cmd(c),angle(t)>
-            x = torch.tensor(img,dtype =torch.float32, device=self.device).unsqueeze(0)
+        # <training mode>
+        self.net.train()
+
+        if self.first_flag:
+            self.x_cat = torch.tensor(
+                img, dtype=torch.float32, device=self.device).unsqueeze(0)
+            self.x_cat = self.x_cat.permute(0, 3, 1, 2)
+            self.t_cat = torch.tensor(
+                [target_angle], dtype=torch.float32, device=self.device).unsqueeze(0)
+            self.first_flag = False
+        # x= torch.tensor(self.transform(img),dtype=torch.float32, device=self.device).unsqueeze(0)
+        # <to tensor img(x),cmd(c),angle(t)>
+        x = torch.tensor(img, dtype=torch.float32,
+                         device=self.device).unsqueeze(0)
         # <(Batch,H,W,Channel) -> (Batch ,Channel, H,W)>
-            x=x.permute(0,3,1,2)
-            t = torch.tensor([target_angle],dtype=torch.float32,device=self.device).unsqueeze(0)
-            self.x_cat =torch.cat([self.x_cat,x],dim=0)
-            self.t_cat =torch.cat([self.t_cat,t],dim=0)
-        
-            
-           
-            #print(self.x_cat.size()[0])
-            
-            
-        #<make dataset>
-            #print("train x =",x.shape,x.device,"train c =" ,c.shape,c.device,"tarain t = " ,t.shape,t.device)
-            dataset = TensorDataset(self.x_cat,self.t_cat)
-        #<dataloder>
-            train_dataset = DataLoader(dataset, batch_size=BATCH_SIZE, generator=torch.Generator('cpu'),shuffle=True)
-            
-        #<only cpu>
-            # train_dataset = DataLoader(dataset, batch_size=BATCH_SIZE,shuffle=True)
-            
-        #<split dataset and to device>
-            for x_train, t_train in train_dataset:
-                x_train.to(self.device,non_blocking=True)
-                t_train.to(self.device,non_blocking=True)
-                break
+        x = x.permute(0, 3, 1, 2)
+        t = torch.tensor([target_angle], dtype=torch.float32,
+                         device=self.device).unsqueeze(0)
+        self.x_cat = torch.cat([self.x_cat, x], dim=0)
+        self.t_cat = torch.cat([self.t_cat, t], dim=0)
 
-        #<learning>
-            # print(t_train)
-            self.optimizer.zero_grad()
-            # self.net.zero_grad()
-            y_train = self.net(x_train)
-            # print(y_train,t_train)
-            loss = self.criterion(y_train, t_train) 
-            loss.backward()
-            # self.optimizer.zero_grad
-            self.optimizer.step()
-            #self.writer.add_scalar("loss",loss,self.count)
-            
-        #<test>
-            self.net.eval()
-            action_value_training = self.net(x)
-            #self.writer.add_scalar("angle",abs(action_value_training[0][0].item()-target_angle),self.count)
-            # print("action=" ,action_value_training[0][0].item() ,"loss=" ,loss.item())
-            # print("action=" ,action_value_training.item() ,"loss=" ,loss.item())
+        # print(self.x_cat.size()[0])
 
-            # if self.first_flag:
-            #     self.writer.add_graph(self.net,(x,c))
-            #self.writer.close()
-            #self.writer.flush()
-        #<reset dataset>
-            if self.x_cat.size()[0] > MAX_DATA:
-                self.x_cat = torch.empty(1,3,48,64).to(self.device)
-                self.target_angles =torch.empty(1,1).to(self.device)
-                self.first_flag=True
-                print("reset dataset")
-            
-            # return action_value_training.item(), loss.item()
-            return action_value_training[0][0].item(), loss.item()
+        # <make dataset>
+        #print("train x =",x.shape,x.device,"train c =" ,c.shape,c.device,"tarain t = " ,t.shape,t.device)
+        dataset = TensorDataset(self.x_cat, self.t_cat)
+        # <dataloder>
+        train_dataset = DataLoader(
+            dataset, batch_size=BATCH_SIZE, generator=torch.Generator('cpu'), shuffle=True)
+
+        # <only cpu>
+        # train_dataset = DataLoader(dataset, batch_size=BATCH_SIZE,shuffle=True)
+
+        # <split dataset and to device>
+        for x_train, t_train in train_dataset:
+            x_train.to(self.device, non_blocking=True)
+            t_train.to(self.device, non_blocking=True)
+            break
+
+        # <learning>
+        # print(t_train)
+        self.optimizer.zero_grad()
+        # self.net.zero_grad()
+        y_train = self.net(x_train)
+        # print(y_train,t_train)
+        loss = self.criterion(y_train, t_train)
+        loss.backward()
+        # self.optimizer.zero_grad
+        self.optimizer.step()
+        # self.writer.add_scalar("loss",loss,self.count)
+
+        # <test>
+        self.net.eval()
+        action_value_training = self.net(x)
+        # self.writer.add_scalar("angle",abs(action_value_training[0][0].item()-target_angle),self.count)
+        # print("action=" ,action_value_training[0][0].item() ,"loss=" ,loss.item())
+        # print("action=" ,action_value_training.item() ,"loss=" ,loss.item())
+
+        # if self.first_flag:
+        #     self.writer.add_graph(self.net,(x,c))
+        # self.writer.close()
+        # self.writer.flush()
+        # <reset dataset>
+        if self.x_cat.size()[0] > MAX_DATA:
+            self.x_cat = torch.empty(1, 3, 48, 64).to(self.device)
+            self.target_angles = torch.empty(1, 1).to(self.device)
+            self.first_flag = True
+            print("reset dataset")
+
+        # return action_value_training.item(), loss.item()
+        return action_value_training[0][0].item(), loss.item()
 
     def act(self, img):
-            self.net.eval()
-        #<make img(x_test_ten),cmd(c_test)>
-            # x_test_ten = torch.tensor(self.transform(img),dtype=torch.float32, device=self.device).unsqueeze(0)
-            x_test_ten = torch.tensor(img,dtype=torch.float32, device=self.device).unsqueeze(0)
-            x_test_ten = x_test_ten.permute(0,3,1,2)
-            #print(x_test_ten.shape,x_test_ten.device,c_test.shape,c_test.device)
-        #<test phase>
-            action_value_test = self.net(x_test_ten)
-            
-            print("act = " ,action_value_test.item())
-            return action_value_test.item()
+        self.net.eval()
+        # <make img(x_test_ten),cmd(c_test)>
+        # x_test_ten = torch.tensor(self.transform(img),dtype=torch.float32, device=self.device).unsqueeze(0)
+        x_test_ten = torch.tensor(
+            img, dtype=torch.float32, device=self.device).unsqueeze(0)
+        x_test_ten = x_test_ten.permute(0, 3, 1, 2)
+        # print(x_test_ten.shape,x_test_ten.device,c_test.shape,c_test.device)
+        # <test phase>
+        action_value_test = self.net(x_test_ten)
+
+        print("act = ", action_value_test.item())
+        return action_value_test.item()
 
     def result(self):
-            accuracy = self.accuracy
-            return accuracy
+        accuracy = self.accuracy
+        return accuracy
 
     def save(self, save_path):
-        #<model save>
+        # <model save>
         path = save_path + time.strftime("%Y%m%d_%H:%M:%S")
         os.makedirs(path)
         torch.save(self.net.state_dict(), path + '/model_gpu.pt')
 
-
     def load(self, load_path):
-        #<model load>
-        self.net.state_dict(torch.load(load_path))
+        # <model load>
+        self.net.load_state_dict(torch.load(load_path))
+
 
 if __name__ == '__main__':
-        dl = deep_learning()
+    dl = deep_learning()
